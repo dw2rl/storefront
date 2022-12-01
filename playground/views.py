e259a63c7e8044fcdb40f1468e89a42cc9f0935b
Umidjon Juraboev <tom74459@gmail.com>
2022-12-01 15:53:26 +0500

    aggregating objects.

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, Order
 
 
 def say_hello(request):
-    queryset = Order.objects.select_related(
-        'customer').order_by('-placed_at').prefetch_related('orderitem_set__product')[:5]
-    return render(request, 'hello.html', {'name': 'Mosh', 'orders': list(queryset)})
+    result = Product.objects.aggregate(Count('id'), Min('unit_price'))
+    return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
